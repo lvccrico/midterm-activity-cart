c51f61f622f922543e9af5c8d3296d2cfb838f31
Rico Custodio <i.am.ricocustodio@gmail.com>
2017-08-31 08:02:58 +0800

    add customer's registration and login

---
+++ app/Customer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name', 'last_name', 'address',
+    ];
+
+    public function user() {
+    	return $this->hasOne('App\User');	
+    }
+}
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -48,7 +49,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:20',
+            'last_name' => 'required|string|max:20',
+            'address' => 'required|string|max:200',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
@@ -62,10 +65,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+        $customer = Customer::create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'address' => $data['address']
         ]);
+
+        $user = new User;
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+
+        return $customer->user()->save($user);
     }
 }
